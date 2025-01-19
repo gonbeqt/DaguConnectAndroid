82f3bc200473d7e70d7836411b82df097dde1691
chiyarrih <charrymaedoria@gmail.com>
2025-01-19 23:51:47 +0800

    bottomnav
    bottomnav

---
+++ app/src/main/java/com/example/androidproject/MainScreen.kt
@@ -0,0 +1,73 @@
+package com.example.androidproject
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.CollectionsBookmark
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Message
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.androidproject.pages.BookmarkedScreen
+import com.example.androidproject.pages.HomeScreen
+import com.example.androidproject.pages.MessageScreen
+import com.example.androidproject.pages.ScheduleScreen
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    val navItems = listOf(
+        NavigationItem("Home", Icons.Default.Home),
+        NavigationItem("Schedule", Icons.Default.CalendarMonth),
+        NavigationItem("Bookmark", Icons.Default.CollectionsBookmark),
+        NavigationItem("Message", Icons.Default.Message)
+
+    )
+    var selectedItem by remember {
+        mutableStateOf(0)
+    }
+
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                navItems.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItem == index,
+                        onClick = { },
+                        icon = {
+                            Icon(imageVector = item.icon, contentDescription = "Icon")
+                        },
+                        label = {
+                            (Text(text = item.nav_label))
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        ContentScreen(modifier = Modifier.padding(innerPadding),selectedItem)
+    }
+}
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier, selectedItem: Int) {
+    when (selectedItem) {
+        0 -> HomeScreen(modifier)
+        1 -> ScheduleScreen(modifier)
+        2 -> BookmarkedScreen(modifier)
+        3 -> MessageScreen(modifier)
+}
+
+
---
+++ app/src/main/java/com/example/androidproject/pages/Schedule.kt
@@ -0,0 +1,34 @@
+package com.example.androidproject.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.sp
+
+
+
+@Composable
+
+fun ScheduleScreen(modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Green),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Schedule Screen",
+            fontSize = 30.sp,
+            color = Color.White
+        )
+    }
+
+}
+
