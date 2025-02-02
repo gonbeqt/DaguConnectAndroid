8475ffb0853db04cbf33425a7b06e6185764aae8
chiyarrih <charrymaedoria@gmail.com>
2025-02-02 18:49:48 +0800

    tradesman's profile
    tradesman profileeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -27,9 +27,11 @@ import com.example.androidproject.ui.theme.views.pages.HomeScreen
 import com.example.androidproject.ui.theme.views.pages.MessageScreen
 import com.example.androidproject.ui.theme.views.pages.Plumbing
 import com.example.androidproject.ui.theme.views.pages.RateAndReviews
+import com.example.androidproject.ui.theme.views.pages2.AvailabilityStatusScreen
 import com.example.androidproject.ui.theme.views.pages2.BookingsTradesman
 import com.example.androidproject.ui.theme.views.pages2.BookmarkedTradesman
 import com.example.androidproject.ui.theme.views.pages2.HomeTradesman
+import com.example.androidproject.ui.theme.views.pages2.ProfileTradesman
 import com.example.androidproject.ui.theme.views.pages2.ScheduleTradesman
 import com.example.androidproject.ui.theme.views.pages2.TradesmanApply
 
@@ -44,7 +46,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidProjectTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = startDestination) {
+                NavHost(navController = navController, startDestination = "profiletradesman") {
                     composable("landing_page") {
                         LandingPageScreen(navController)
                     }
@@ -113,6 +115,10 @@ class MainActivity : ComponentActivity() {
                     composable("scheduletradesman") {
                         ScheduleTradesman(modifier = Modifier,navController)
                     }
+                    composable("profiletradesman") {
+                        ProfileTradesman(modifier = Modifier,navController)
+                    }
+
 
                 }
             }
