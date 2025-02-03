5e14f92f67a4b13943cccaa74b99547ac76a5919
NooisY <143975472+lN00ISYl@users.noreply.github.com>
2025-02-03 20:05:57 +0800

    added the getbooking for all items

---
+++ app/src/main/java/com/example/androidproject/viewmodel/Resumes/GetResumesViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.androidproject.viewmodel.Resumes
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.androidproject.api.ApiService
+import com.example.androidproject.data.preferences.TokenManager
+import com.example.androidproject.model.resumesItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class GetResumesViewModel(private val apiService: ApiService, private val context: Context):ViewModel() {
+    private val _resumeState = MutableStateFlow<ResumeState>(ResumeState.Idle)
+    val resumeState = _resumeState.asStateFlow()
+
+
+    fun getResumes(){
+        viewModelScope.launch {
+            _resumeState.value = ResumeState.Loading
+            try {
+                val token = TokenManager.getToken()
+                val response = apiService.getResumes("Bearer $token" )
+                val body = response.body()
+                if (response.isSuccessful) {
+                    if (body != null) {
+                        _resumeState.value = ResumeState.Success(body.resumes)
+                    } else {
+                        _resumeState.value = ResumeState.Error("No resumes found.")
+                    }
+                } else {
+                    _resumeState.value = ResumeState.Error("Failed to retrieve resumes.")
+                }
+            } catch (e: Exception) {
+                _resumeState.value = ResumeState.Error(e.message ?: "An error occurred.")
+            }
+        }
+    }
+
+
+    sealed class ResumeState(){
+        object Idle : ResumeState()
+        object Loading : ResumeState()
+        data class Success(val data: ArrayList<resumesItem>) : ResumeState()
+        data class Error(val message: String) : ResumeState()
+
+    }
+}
---
+++ app/src/main/java/com/example/androidproject/viewmodel/bookings/GetClientBookingViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.androidproject.viewmodel.bookings
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.androidproject.api.ApiService
+import com.example.androidproject.data.preferences.TokenManager
+import com.example.androidproject.model.client.GetClientsBooking
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class GetClientBookingViewModel(private val apiService: ApiService, private val context: Context) : ViewModel() {
+    private val _clientbookingState = MutableStateFlow<GetClientBookings>(GetClientBookings.Idle)
+    val clientbookingState = _clientbookingState.asStateFlow()
+
+    fun getClientBookings() {
+        viewModelScope.launch {
+            _clientbookingState.value = GetClientBookings.Loading
+            try {
+                val token = TokenManager.getToken()
+                val response = apiService.getClientBooking("Bearer $token")
+                val body = response.body()
+                if (response.isSuccessful) {
+                    if (body != null) {
+                        _clientbookingState.value = GetClientBookings.Success(body)
+                    }
+                } else {
+                    _clientbookingState.value = GetClientBookings.Error(response.message())
+                }
+            } catch (e: Exception) {
+                _clientbookingState.value = GetClientBookings.Error(e.localizedMessage ?: "Unknown error")
+            }
+        }
+    }
+
+    sealed class GetClientBookings{
+        object Idle : GetClientBookings()
+        object Loading : GetClientBookings()
+        data class Success(val data: List<GetClientsBooking>) : GetClientBookings()
+        data class Error(val message: String) : GetClientBookings()
+
+    }
+}
