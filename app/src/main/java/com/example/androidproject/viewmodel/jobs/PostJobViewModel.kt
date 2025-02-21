13ca2140ffa57f8e77932a205ae311b57988b74b
Auxilium <PaulVillacote.PGMR@gmail.com>
2025-02-21 22:23:53 +0800

    Update PostJobViewModel.kt

--- app/src/main/java/com/example/androidproject/viewmodel/jobs/PostJobViewModel.kt
+++ app/src/main/java/com/example/androidproject/viewmodel/jobs/PostJobViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.androidproject.viewmodel.jobs
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.androidproject.api.ApiService
 import com.example.androidproject.model.PostJobResponse
+import com.example.androidproject.model.RequestJobs
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -16,7 +18,22 @@ class PostJobViewModel(private val apiService: ApiService) : ViewModel()  {
         viewModelScope.launch {
             _postJobState.value = PostJobState.Loading
             try {
-
+                val response = apiService.postJob(RequestJobs(salary, applicantLimitCount, jobType, jobDescription, address, status, deadline))
+                if (response.isSuccessful) {
+                    val message = response.body()
+                    if (message != null) {
+                        _postJobState.value = PostJobState.Success(message)
+                    } else {
+                        _postJobState.value = PostJobState.Error("Response body is null")
+                        Log.e("PostJobViewModel", "Response body is null")
+                    }
+                } else {
+                    _postJobState.value = PostJobState.Error("Error: ${response.code()}")
+                    Log.e("PostJobViewModel", "Exception: ${response.code()}")
+                }
+            } catch (e: Exception) {
+                _postJobState.value = PostJobState.Error("Exception: ${e.message}")
+                Log.e("PostJobViewModel", "Exception: ${e.message}", e)
             }
         }
     }
